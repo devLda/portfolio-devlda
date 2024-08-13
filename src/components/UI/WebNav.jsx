500885ebfd16583e42eca2e6c7a94008433ef1a2
devLda <leducanh.mywork@gmail.com>
2024-08-14 00:04:37 +0700

    fix bug giao dien nav

--- src/components/UI/WebNav.jsx
+++ src/components/UI/WebNav.jsx
@@ -17,13 +17,13 @@ const WebNav = () => {
             key={index}
             to={item.path}
             className={`
-              relative flex items-center justify-center w-[100px] h-[40px] text-xl z-0 border border-yellow-500 border-solid duration-500 ${
+              relative flex items-center justify-center w-[100px] h-[40px] text-xl z-0 border border-yellow-500 border-solid ease-out duration-1000 ${
                 pathName.includes(item.path)
                   ? "bg-amber-50 text-yellow-500 before:w-0 after:h-0 before:bg-amber-50 after:bg-amber-50"
-                  : "hover:bg-amber-50 hover:text-yellow-500 hover:before:w-0 hover:after:h-0 before:hover:bg-amber-50 after:hover:bg-amber-50"
+                  : "hover:bg-amber-50 hover:text-yellow-500 hover:before:w-0 before:h-[40px] before:bg-amber-100 before:hover:bg-amber-50 hover:after:h-0 after:w-[100px] after:bg-amber-100 after:hover:bg-amber-50"
               } 
-               before:content-[''] before:absolute before:w-2/3 before:h-[40px] before:-z-10 before:bg-amber-100  before:duration-500
-               after:content-[''] after:absolute after:w-[100px] after:h-2/3 after:-z-10 after:bg-amber-100  after:duration-500
+               before:content-[''] before:absolute before:w-2/3 before:-z-10 before:ease-out before:duration-1000
+               after:content-[''] after:absolute after:h-2/3 after:-z-10 after:ease-out after:duration-1000
             `}
           >
             {/* 
